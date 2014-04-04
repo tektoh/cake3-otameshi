eee72157a706a8a3a51966e57a52d073660791d6
Shinichi Segawa <tektoh@gmail.com>
2014-04-04 21:53:59 +0900

    Authを作りたい

--- App/Controller/UsersController.php
+++ App/Controller/UsersController.php
@@ -5,24 +5,63 @@ use Cake\Core\Configure;
 use Cake\Error;
 use Cake\Utility\Inflector;
 use Cake\ORM\TableRegistry;
+use Cake\Event\Event;
 use App\Model\Entity\User;
 
 class UsersController extends AppController {
+    public $components = [
+        'Auth' => [
+            'loginAction' => [
+                'controller' => 'users',
+                'action' => 'login',
+            ],
+            'loginRedirect' => [
+                'controller' => 'users',
+                'action' => 'index',
+            ],
+            'logoutRedirect' => [
+                'controller' => 'users',
+                'action' => 'login',
+            ],
+            'authError' => 'Did you really think you are allowed to see that?',
+            'authenticate' => [
+                'Form' => [
+                    'fields' => ['username' => 'username'],
+                    'passwordHasher' => [
+                        'className' => 'Blowfish' // 3.0のデフォルトはBlowfish
+                    ]
+                ]
+            ]
+        ]
+    ];
+
+    public function beforeFilter(Event $event) {
+        $this->Auth->allow('register');
+    }
+
   public function index() {
     $users = $this->Users->find('all');
     $this->set(compact('users'));
   }
 
   public function login() {
+      if ($this->request->is('post')) {
+          if ($this->Auth->login()) {
+              return $this->redirect($this->Auth->redirectUrl());
+          } else {
+              $this->Session->setFlash('Username or password is incorrect', 'default', [], 'auth');
+          }
+      }
   }
 
-  public function register() {
-    if ($this->request->is('post')) {
-      $user = $this->Users->newEntity($this->request->data);
-      if (!$this->Users->save($user)) {
-        throw new Error\InternalErrorException('Error');
-      }
+    public function register() {
+        if ($this->request->is('post')) {
+            $user = $this->Users->newEntity($this->request->data);
+            if (!$this->Users->save($user)) {
+                throw new Error\InternalErrorException('Error');
+            }
+            return $this->redirect($this->Auth->loginAction);
+        }
     }
-  }
 
 }
--- App/Model/Table/UserTable.php
+++ App/Model/Table/UserTable.php
@@ -2,8 +2,17 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Controller\Component\Auth\BlowfishPasswordHasher;
+use Cake\Event\Event;
 
 class UsersTable extends Table {
 
+    public function beforeSave(Event $event, Entity $entity, array $options) {
+        if (!$this->id) {
+            $passwordHasher = new BlowfishPasswordHasher();
+            $this->data['User']['password'] = $passwordHasher->hash($this->data['User']['password']);
+        }
+        return true;
+    }
 }
 
