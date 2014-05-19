88395d152c00635f4066207695ead8c4c82185d9
Shinichi Segawa <tektoh@gmail.com>
2014-05-19 20:29:45 +0900

    cakephp3 dev3

--- script.sh
+++ script.sh
@@ -12,6 +12,11 @@ else
   /usr/local/bin/composer install
 fi
 
+# COPY app.php
+if [ ! -f App/Config/app.php ]; then
+  cp -v App/Config/app{.default,}.php
+else
+
 # CREATE DATABASE
 mysql -u root -psecret -e "CREATE DATABASE my_app DEFAULT CHARSET utf8" > /dev/null 2>&1
 mysql -u root -psecret -e "CREATE DATABASE test_myapp DEFAULT CHARSET utf8" > /dev/null 2>&1
